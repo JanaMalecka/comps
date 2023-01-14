4c9b5339d67915a3bd889e4c8846bc62fa3bd12e
Jana Malecka <jm.jmalecka@gmail.com>
2023-01-14 15:39:00 +0100

    Buttons, Accordion - using Tailwind, classnames

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,42 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from '../components/Button';
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log('click');
+  };
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy me
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See deal
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide ads
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
