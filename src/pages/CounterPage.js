db8828f949f73d8a5738e677700ec02397241aba
Jana Malecka <jm.jmalecka@gmail.com>
2023-02-03 16:25:24 +0100

    useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,4 +1,124 @@
+import { useReducer } from 'react';
 import Button from '../components/Button';
+import Panel from '../components/Panel';
+
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'change_value_to_add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+
+    default:
+      return state;
+    // throw new Error('unexpected action type: ' + action.type)
+  }
+  /* 
+  switch instead of ifs
+  if (action.type === INCREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === SET_VALUE_TO_ADD) {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+
+  return state; */
+};
+
+function CounterPage({ initialCount }) {
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  // console.log(state); for debugging
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0; //if NaN than 0
+
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+  };
+
+  return (
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+          onChange={handleChange}
+          value={state.valueToAdd || ''} //if I get 0 as a state than display empty String in input
+        />
+
+        <Button> Add it!</Button>
+      </form>
+    </Panel>
+  );
+}
+
+export default CounterPage;
+
+/* 
+useState + custom hook
+
+import Button from '../components/Button'; useState + custom hook
 import useCounter from '../hooks/use-counter';
 
 function CounterPage({ initialCount }) {
@@ -12,4 +132,59 @@ function CounterPage({ initialCount }) {
   );
 }
 
-export default CounterPage;
+export default CounterPage; */
+
+/* 
+using useState instead of useReducer
+
+import { useState } from 'react';
+import Button from '../components/Button';
+import Panel from '../components/Panel';
+
+function CounterPage({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0; //if NaN than 0
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
+
+  return (
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+          onChange={handleChange}
+          value={valueToAdd || ''} //if I get 0 as a state than display empty String in input
+        />
+
+        <Button> Add it!</Button>
+      </form>
+    </Panel>
+  );
+}
+
+export default CounterPage; */
